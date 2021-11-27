05c19c07804cc6c61508e863ec22167799fe0ffc
Nils Schikora <nils@schikora-it.de>
2021-11-27 21:08:22 +0100

    Let the user drag his marker around

--- src/usePlayerCanvas.js
+++ src/usePlayerCanvas.js
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react";
 import rough from "roughjs/bundled/rough.cjs";
 
 // const axisOffset = 20;
-// const line = { stroke: "#212121" };
-// const dashedLine = { strokeLineDash: [15, 8], stroke: "#b0bec5" };
+// const line = { stroke: '#212121' };
+// const dashedLine = { strokeLineDash: [15, 8], stroke: '#b0bec5' };
 
 function usePlayerCanvas(dx, dy, size, canvasRef) {
   const [canvas, setCanvas] = useState(null);
   const [ctx, setCtx] = useState(null);
+  const [mouseOnCircle, setMouseOnCircle] = useState(false);
+  const [dragging, setDragging] = useState(false);
+  const [[x, y], setXY] = useState([dx + 100, dy + 100]);
 
   useEffect(() => {
     if (!canvasRef.current) {
@@ -18,18 +21,52 @@ function usePlayerCanvas(dx, dy, size, canvasRef) {
   }, [canvasRef]);
 
   useEffect(() => {
-    if (!ctx) {
+    if (!canvas || !ctx) {
       return;
     }
-  }, [ctx]);
+
+    const moveHandler = (event) => {
+      const crc = new Path2D();
+      crc.arc(x, y, 35, 0, 2 * Math.PI);
+
+      if (dragging) {
+        setXY([event.offsetX, event.offsetY]);
+      } else if (ctx.isPointInPath(crc, event.offsetX, event.offsetY)) {
+        setMouseOnCircle(true);
+      } else {
+        setMouseOnCircle(false);
+      }
+    };
+
+    const downHandler = (event) => {
+      setDragging(true);
+      setXY([event.offsetX, event.offsetY]);
+    };
+
+    const upHandler = (event) => {
+      setDragging(false);
+      setXY([event.offsetX, event.offsetY]);
+    };
+
+    canvas.canvas.addEventListener("mousemove", moveHandler);
+    canvas.canvas.addEventListener("mousedown", downHandler);
+    canvas.canvas.addEventListener("mouseup", upHandler);
+
+    return () => {
+      canvas.canvas.removeEventListener("mousemove", moveHandler);
+    };
+  }, [ctx, canvas, dx, dy, dragging, x, y]);
 
   useEffect(() => {
-    if (!canvas) {
+    if (!canvas || !ctx) {
       return;
     }
 
-    canvas.circle(dx + 100, dy + 100, 35, { fill: "#4db6ac" });
-  }, [canvas, dx, dy]);
+    ctx.clearRect(0, 0, canvas.canvas.width, canvas.canvas.height);
+    canvas.circle(x, y, 35, {
+      fill: mouseOnCircle ? "#8e24aa" : "#4db6ac",
+    });
+  }, [canvas, ctx, dx, dy, mouseOnCircle, x, y]);
 }
 
 export default usePlayerCanvas;
